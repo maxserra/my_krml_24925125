21907638b6416a1868fa99bd0d685c8f186c5b1f
Max Serra <mserralasierra@gmail.com>
2023-08-29 20:57:12 +1000

    fix scale_and_encode function logic

--- src/my_krml_24925125/features/transformations.py
+++ src/my_krml_24925125/features/transformations.py
@@ -8,7 +8,7 @@ def scale_and_encode(data: pd.DataFrame,  # only features, no target needed here
                      scaler = None,
                      encoder = None,
                      ignore_cols: List[str] = None,
-                     return_transformers: bool = True) -> pd.DataFrame:
+                     return_transformers: bool = True):
 
     temp_data = data.copy()
 
@@ -19,22 +19,29 @@ def scale_and_encode(data: pd.DataFrame,  # only features, no target needed here
     numeric_cols = list(temp_data.select_dtypes("number").columns)
     categorical_cols = list(set(temp_data.columns) - set(numeric_cols))
 
-    scaler = scaler if scaler is not None else StandardScaler()
-    encoder = encoder if encoder is not None else OneHotEncoder(
-        drop="first",
-        sparse_output=False,
-        dtype=int,
-        handle_unknown="infrequent_if_exist"
-    )
+    if scaler is None:
+        temp_scaler = StandardScaler()
+        temp_scaler.fit(temp_data[numeric_cols])
+    else:
+        temp_scaler = scaler
 
-    out_data = encoder.fit_transform(temp_data[categorical_cols])
-    out_data = pd.DataFrame(out_data, columns=encoder.get_feature_names_out())
-    out_data[numeric_cols] = scaler.fit_transform(temp_data[numeric_cols])
+    if encoder is None:
+        temp_encoder = OneHotEncoder(drop="first",
+                                     sparse_output=False,
+                                     dtype=int,
+                                     handle_unknown="infrequent_if_exist")
+        temp_encoder.fit(temp_data[categorical_cols])
+    else:
+        temp_encoder = encoder
+
+    out_data = temp_encoder.transform(temp_data[categorical_cols])
+    out_data = pd.DataFrame(out_data, columns=temp_encoder.get_feature_names_out())
+    out_data[numeric_cols] = temp_scaler.transform(temp_data[numeric_cols])
 
     if ignore_cols is not None:
         out_data[ignore_cols] = ignore_data
     
     if return_transformers:
-        return out_data, scaler, encoder
+        return out_data, temp_scaler, temp_encoder
     else:
         return out_data
