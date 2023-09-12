0d01b7729f87ec68b1ecd406b16f17db22d5f662
Max Serra <mserralasierra@gmail.com>
2023-09-12 13:03:22 +1000

    fix float indexing mistake

--- src/my_krml_24925125/models/null.py
+++ src/my_krml_24925125/models/null.py
@@ -79,7 +79,7 @@ class NullClassifier:
 
     def fit(self, y):
         self.y = y
-        self.pred_value = mode(y)[0][0]
+        self.pred_value = mode(y)
 
     def predict(self, y):
         self.preds = np.full((len(y), 1), self.pred_value)
